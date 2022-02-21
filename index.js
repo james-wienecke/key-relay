3b28a28121cd56a21aee77833a720c4e89cf03e1
James Wienecke <james.a.wienecke@gmail.com>
2022-02-21 12:08:39 -0600

    Set up movies route.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3001;
+const movies = require('./movies')
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/movies", movies);
 
 // test route
 app.get("/", (req, res) => res.json( { success: "Hello world!" }));
