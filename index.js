514091ae8c880569fadeb7fefecbcda158d40764
James Wienecke <james.a.wienecke@gmail.com>
2022-02-21 13:19:02 -0600

    Added whitelisting with cors.

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const port = 3001;
 const movies = require('./movies')
 
 app.use(express.json());
-app.use(cors());
+
+const whitelist = ['http://localhost', 'http://127.0.0.1:3000', 'http://127.0.0.1:8080'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Denied"))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 const limiter = rateLimit({
     windowMs: 500,
     max: 1,
 });
+app.use(limiter);
 
 app.use("/movies", movies);
 
