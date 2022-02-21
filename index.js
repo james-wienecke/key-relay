80a80893b4c69265ed474a0d4a14af60f9ba0f36
James Wienecke <james.a.wienecke@gmail.com>
2022-02-21 12:50:52 -0600

    Added rate limit.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 const app = express();
 const port = 3001;
@@ -7,6 +8,11 @@ const movies = require('./movies')
 
 app.use(express.json());
 app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 500,
+    max: 1,
+});
 
 app.use("/movies", movies);
 
